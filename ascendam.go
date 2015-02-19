6c26ae755a9ad7182b35eaa9deb0f8abe0084d8b
Stig Lindqvist <stojg.lindqvist@gmail.com>
2015-02-20 10:37:44 +1300

    Better capture of network and timout errors

--- ascendam.go
+++ ascendam.go
@@ -1,24 +1,20 @@
 package main
 
 import (
-	"net"
 	"net/http"
 	"fmt"
 	"os"
 	"flag"
-	"time"
 	"log"
+	"time"
+	"io/ioutil"
+	"crypto/tls"
+	"github.com/mreiferson/go-httpclient"
 )
 
 var Usage = func() {
 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
 	flag.PrintDefaults()
-}
-
-var timeout_ms = time.Duration(30 * time.Second)
-
-func dialTimeout(network, addr string) (net.Conn, error) {
-	return net.DialTimeout(network, addr, timeout_ms)
 }
 
 func main() {
@@ -44,55 +40,70 @@ func main() {
 
 	timeout_ms := time.Millisecond * time.Duration(max_time_ms)
 
-	transport := http.Transport{
-		Dial: dialTimeout,
+	var transport = &httpclient.Transport {
+		ConnectTimeout: timeout_ms,
+		ResponseHeaderTimeout: timeout_ms,
+		RequestTimeout: timeout_ms,
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 
-	client := http.Client{
-		Transport: &transport,
+	var client = &http.Client{
+		Transport: transport,
 	}
 
 	fmt.Printf("Running uptime check on '%s'\n", url)
 
 	fmt.Printf("Timeout is set to %s\n", timeout_ms)
 
-	server_up := false
+	last_state := true
+
+	var message string
 
 	for {
-		failed := false
+		state := false
 
 		// fetch page
 		start := time.Now()
-		resp, err := client.Get(url)
+		code, err := getCode(url, client)
 		elapsed := time.Since(start)
 
-		down_message := ""
-
 		if err != nil {
-			down_message = fmt.Sprintf("Down\t%s\t%s\t\t%s", "n/a", elapsed, err)
-			failed = true
+			message = fmt.Sprintf("Down\t%s\t%s\t\t%s", "n/a", elapsed, err)
+			state = true
 			time.Sleep(50 * time.Millisecond)
+		} else if code != 200 {
+			message = fmt.Sprintf("Down\t%d\t%s", code, elapsed)
+			state = true
+		} else  if timeout_ms > 0 && elapsed > timeout_ms {
+			message = fmt.Sprintf("Down\t%d\t%s", code, elapsed)
+			state = true
 		} else {
-			defer resp.Body.Close()
-
-			if resp.StatusCode != 200 {
-				down_message = fmt.Sprintf("Down\t%d\t%s", resp.StatusCode, elapsed)
-				failed = true
-			}
-
-			if timeout_ms > 0 && elapsed > timeout_ms {
-				down_message = fmt.Sprintf("Down\t%d\t%s", resp.StatusCode, elapsed)
-				failed = true
-			}
+			message = fmt.Sprintf("Up\t%d\t%s", code, elapsed)
 		}
 
-		if failed && server_up {
-			server_up = false
-			log.Print(down_message)
-		} else if !failed && !server_up {
-			server_up = true
-			log.Printf("Up\t%d\t%s", resp.StatusCode, elapsed)
+		if last_state != state {
+			log.Print(message)
+			last_state = state
 		}
 	}
+}
 
+func getCode(url string, client *http.Client) (code int, err error) {
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("User-Agent", "MustWin/health-checker")
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return 0, err
+	}
+	defer func() {
+		resp.Body.Close()
+	}()
+
+	_, err = ioutil.ReadAll(resp.Body)
+	if (err != nil) {
+		return 0, err
+	}
+
+	return resp.StatusCode, nil
 }
